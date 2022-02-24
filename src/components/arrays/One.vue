da8fc721cf96a1dd3ee5936c7a9e4353735d8eb3
Pawan Bhattarai <pawan.bhattarai@devk.de>
2022-02-24 21:17:43 +0100

    adds question

--- src/components/arrays/One.vue
+++ src/components/arrays/One.vue
@@ -2,19 +2,29 @@
   <div class="container">
     <h1>Arrays</h1>
     <hr />
+    <b>Exercise Two: </b> Display a list of all number between 650 and 5000 which are divisible by 217.
+    <hr />
+    <b>Answer:</b>
+    <ul>
+      <li>217</li>
+    </ul>
+
+    <hr />
     <b>Exercise One: </b> Display a list of all number between 1 and 500 which are divisible by 17.
     <hr />
     <b>Answer:</b>
 
     <ul>
-      <li v-for="number in numberList()">{{number}}</li>
+      <li v-for="number in numbersDivisibleBySeventeen()">{{number}}</li>
     </ul>
+
+
   </div>
 </template>
 
 <script setup>
 
-  function numberList() {
+  function numbersDivisibleBySeventeen() {
     let numbers = [];
 
     for (let i = 0; i <= 500; i++) {
@@ -25,7 +35,6 @@
     console.log(numbers);
     return numbers;
   }
-
 
 </script>
 
